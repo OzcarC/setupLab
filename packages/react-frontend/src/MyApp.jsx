a37fca6517d876ddf42d07e0624ac29f9eccd19a
Ozcar Cayetano <ozcar@Ozcars-MacBook-Pro.local>
2025-04-15 21:09:44 -0700

    adding waits for backend to process to show on front

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -18,15 +18,33 @@ function MyApp() {
     return promise;
   }
 
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
     });
     setCharacters(updated);
   }
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
+
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
+}
+
   return (
     <div className="container">
       <Table
